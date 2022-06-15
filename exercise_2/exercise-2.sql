d38c57c46a761d31091a4072bf78a8977ef02b1a
Kamie Short <kamie.short@gmail.com>
2022-06-15 14:11:43 -0700

    test 3 passing

--- exercise_2/exercise-2.sql
+++ exercise_2/exercise-2.sql
@@ -1,7 +1,7 @@
 -- find all rental dates for customers with the first_name Patricia
 -- use INNER JOIN FROM customer onto rental
 
-SELECT rental.rental_id, rental.rental_date
+SELECT rental.rental_id
 FROM rental 
 INNER JOIN customer
 
