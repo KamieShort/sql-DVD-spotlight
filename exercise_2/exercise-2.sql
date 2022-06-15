d6c45ebedd9634e6d53889a0bb9e35e451e64696
Kamie Short <kamie.short@gmail.com>
2022-06-15 12:57:49 -0700

    exercise 1 test passing

--- exercise_2/exercise-2.sql
+++ exercise_2/exercise-2.sql
@@ -1,2 +1,8 @@
 -- find all rental dates for customers with the first_name Patricia
 -- use INNER JOIN FROM customer onto rental
+
+Drop table if exists city;
+Drop table if exists address;
+
+SELECT 
+
